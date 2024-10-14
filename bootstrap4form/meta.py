2ac77f8e183d42058dd52fda79ebbdbd14b5262d
Georg Lehner <jorge@magma-soft.at>
2024-10-14 16:33:22 +0200

    Update to support python3 > 3.12

--- bootstrap4form/meta.py
+++ bootstrap4form/meta.py
@@ -1,5 +1,7 @@
-from distutils.version import StrictVersion
-
-
-VERSION = StrictVersion('4.0.2')
-
+from packaging.version import Version, parse
+from platform import python_version
+if Version(python_version()) >= Version('3.10'):
+    VERSION = Version('4.0.2')
+else:
+    from distutils.version import StrictVersion
+    VERSION = StrictVersion('4.0.2')
